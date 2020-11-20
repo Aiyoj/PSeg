adeaed6321daa7511574ca612125e3c6b6238ad1
xushiqi <xushiqitc@163.com>
2020-11-20 22:34:27 +0800

    Add flip image augment

---
+++ pseg/dataflow/imgaug/flip.py
@@ -0,0 +1,22 @@
+import cv2
+import random
+
+
+class Fliplr(object):
+    def __init__(self, p=0.5):
+        self.p = p
+
+    def __call__(self, data):
+        if random.random() < self.p:
+            return data
+
+        image = data["image"]
+        label = data["label"]
+
+        h_image = cv2.flip(image, 1)
+        h_label = cv2.flip(label, 1)
+
+        data["image"] = h_image
+        data["label"] = h_label
+
+        return data
