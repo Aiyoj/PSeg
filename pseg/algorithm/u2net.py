c03fe8e6111bdcafe08ab7b2d99f38c2a420855d
xushiqi <xushiqitc@163.com>
2020-11-27 16:26:22 +0800

    Update u2net algorithm

--- pseg/algorithm/u2net.py
+++ pseg/algorithm/u2net.py
@@ -2,8 +2,8 @@ import torch.nn as nn
 
 from addict import Dict
 
-from pseg.modeling.backbone.u2net_backbone import U2NetBackbone
-from pseg.modeling.head.u2net_head import U2NetHead
+from pseg.modeling.backbone.u2net_backbone import U2NetBackbone, U2NetBackboneV1
+from pseg.modeling.head.u2net_head import U2NetHead, U2NetHeadV1
 
 
 class U2Net(nn.Module):
@@ -12,15 +12,13 @@ class U2Net(nn.Module):
 
         self.cfg = Dict(config)
 
-        self.model_name = self.cfg.get("model_name", "small")
-
         self.backbone_type = self.cfg.backbone.get("type", "U2NetBackbone")
         self.backbone_args = self.cfg.backbone.get(
-            "args", {"in_ch": 3, "model_name": self.model_name}
+            "args", {"in_ch": 3, "model_name": "large"}
         )
         self.head_type = self.cfg.head.get("type", "U2NetHead")
         self.head_args = self.cfg.head.get(
-            "args", {"out_ch": 1, "model_name": self.model_name}
+            "args", {"out_ch": 1, "model_name": "large"}
         )
 
         self.backbone = eval(self.backbone_type)(self.backbone_args)
@@ -30,3 +28,14 @@ class U2Net(nn.Module):
         x = self.backbone(x)
         x = self.head(x)
         return x
+
+
+if __name__ == "__main__":
+    import torch
+
+    model = U2Net({
+        "backbone": {"type": "U2NetBackboneV1", "args": {"in_ch": 3, "model_name": "large"}},
+        "head": {"type": "U2NetHeadV1", "args": {"out_ch": 1, "model_name": "large"}}
+    })
+
+    model(torch.ones(1, 3, 640, 640))
