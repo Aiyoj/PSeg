080c2c89842e54440b63b17de6d7d7788d4d5224
xushiqi <xushiqitc@163.com>
2020-12-04 14:34:47 +0800

    Update u2net train

--- experiment/u2net_train_large.py
+++ experiment/u2net_train_large.py
@@ -3,39 +3,63 @@ if __name__ == "__main__":
 
     from experiment.data_loader import DataLoader
     from pseg.algorithm.u2net import U2Net
-    from pseg.modeling.loss.u2net_loss import U2NetLossV1, U2NetLossV2, U2NetLossV3
+    from pseg.modeling.loss.u2net_loss import U2NetLossV1, U2NetLossV2, U2NetLossV3, U2NetLossV4, U2NetLossV5, \
+        U2NetLossV6
     from pseg.engine.optimizer_scheduler import OptimizerScheduler
-    from pseg.engine.learning_rate import CosineDecayWithWarmupLearningRate, ConstantWithWarmupLearningRate
+    from pseg.engine.learning_rate import CosineDecayWithWarmupLearningRate, ConstantWithWarmupLearningRate, \
+        ConstantLearningRate
     from pseg.engine.model_saver import ModelSaver
     from pseg.engine.checkpoint import Checkpoint
     from pseg.utils.log import Logger
     from pseg.engine.trainer import Trainer
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "1,6"
 
-    epochs = 100
+    epochs = 200
     lr = 0.001
-    # save_interval = 331
-    # log_interval = 331
-    batch_size = 10
+    batch_size = 24
 
-    model = U2Net({"model_name": "large"})
+    model_config = {
+        "backbone": {
+            "type": "U2NetBackboneV3", "args": {"in_ch": 3, "model_name": "large"}
+        },
+        "head": {
+            "type": "U2NetHeadV3", "args": {"out_ch": 1, "model_name": "large"}
+        }
+    }
+    model = U2Net(model_config)
 
-    criterion = U2NetLossV2()
-    # criterion = U2NetLossV3()
+    criterion = U2NetLossV6()
+
+    pre_processes = [
+        {"type": "AugHSV", "args": {"p": 0.5}},
+        # {"type": "AugNoise", "args": {"p": 0.5}},
+        {"type": "AugGray", "args": {"p": 0.5}},
+        {"type": "AugBlur", "args": {"p": 0.5}},
+        # {"type": "AugMotionBlur", "args": {"p": 0.5}},
+        {"type": "FlipLR", "args": {"p": 0.5}},
+        {"type": "Affine", "args": {"rotate": [-30, 30]}},
+        {"type": "RandomCropV3", "args": {"size1": (640, 640), "size2": (576, 576)}},
+        {"type": "Perspective", "args": {"p": 0.5}},
+        {"type": "ThinPlateSpline", "args": {"p": 0.5}},
+        {"type": "Normalize", "args": {}},
+        # {"type": "MakeDistMap", "args": {}}
+    ]
 
     train_data_loader = DataLoader(
-        data_dir=["/data2/xushiqi/seg_data/supervisely-person-datasets"],
-        data_list=["/data2/xushiqi/seg_data/supervisely-person-datasets/train.txt"],
+        data_dir=["/input0/LV-MHP-v2", "/input0/humanparsing-atr", "/input0/PortraitMatting"],
+        data_list=["/input0/LV-MHP-v2/train.txt", "/input0/humanparsing-atr/train.txt",
+                   "/input0/PortraitMatting/train.txt"],
         batch_size=batch_size,
         num_worker=4,
         is_training=True,
         remainder=False,
         buffer_size=4,
+        pre_processes=pre_processes
     )
 
-    save_interval = len(train_data_loader)
-    log_interval = len(train_data_loader) // 2
+    save_interval = len(train_data_loader) * 2
+    log_interval = len(train_data_loader)
 
     optimizer_scheduler = OptimizerScheduler(
         {
@@ -58,11 +82,17 @@ if __name__ == "__main__":
     #     }
     # )
 
-    learning_rate = ConstantWithWarmupLearningRate(
+    # learning_rate = ConstantWithWarmupLearningRate(
+    #     {
+    #         "lr": lr,
+    #         "epochs": epochs,
+    #         "warmup_step": len(train_data_loader)
+    #     }
+    # )
+
+    learning_rate = ConstantLearningRate(
         {
-            "lr": lr,
-            "epochs": epochs,
-            "warmup_step": len(train_data_loader)
+            "lr": lr
         }
     )
 
@@ -79,17 +109,17 @@ if __name__ == "__main__":
         {
             "start_epoch": 0,
             "start_iter": 0,
-            "resume": "/data4/xushiqi/projects/PSeg/workspace/PSeg_BCE_Large_1119/u2net/model_epoch_38_minibatch_10830"
+            # "resume": "models/new_u2net.pth"
         }
     )
 
     logger = Logger(
         {
-            "log_dir": "/data2/xushiqi/workspace",
+            "log_dir": "/output/workspace",
             "verbose": False,
             "level": "info",
             "log_interval": log_interval,
-            "name": "PSeg_BCE_Large_1119",
+            "name": "GL_PSegV3_BCE_Large",
             "debug": False
         }
     )
@@ -97,6 +127,6 @@ if __name__ == "__main__":
     trainer = Trainer(
         model=model, criterion=criterion, optimizer_scheduler=optimizer_scheduler,
         learning_rate=learning_rate, model_saver=model_saver,
-        checkpoint=checkpoint, logger=logger
+        checkpoint=checkpoint, logger=logger, frozen_bn=False
     )
     trainer.train(data_loader=train_data_loader, epochs=epochs)
